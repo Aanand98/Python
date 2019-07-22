ce4d4f6bbcc29d2fb5de72f4686152331246210a
Aanand98 <53144307+Aanand98@users.noreply.github.com>
2019-07-22 18:18:54 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,7 @@
+vowel = ['a','e','i','o','u']
+constant = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+n = input()
+if n in vowel or n in constant:
+  print("Alphabel")
+else:
+  print("No")
