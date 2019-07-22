86b87f30a4e14106beb92ca0d7ad6e191756fd1e
Aanand98 <53144307+Aanand98@users.noreply.github.com>
2019-07-22 19:40:54 +0530

    Create odd.py

---
+++ odd.py
@@ -0,0 +1,11 @@
+list = []
+list_values = input()
+list_values = list_values.split()
+for num in list_values:
+    num = int(num)
+    list.append(num)
+start = list[0]
+end = list[-1]
+for i in range(start+1,end):
+    if i % 2 != 0:
+        print("",i,end = '')
